0b7af1ec735a35392084ad5767b520e0312d6056
leeyupeng <leeyupeng@126.com>
2016-11-09 15:42:26 +0800

    YH

---
+++ Editor/Build/ToolsBuildPre.cs
@@ -0,0 +1,24 @@
+/*********************************************************************************
+ *Copyright(C) 2015 by LiYupeng
+ *All rights reserved.
+ *FileName:     ToolsBuildPre.cs
+ *Author:       LiYupeng
+ *Version:      1.0
+ *UnityVersion：5.4.0f3
+ *Date:         2016-11-09
+ *Description:   
+ *History:  
+**********************************************************************************/
+using UnityEngine;
+using System.Collections;
+
+using UnityEditor;
+
+public class ToolsBuildPre :Editor {
+
+    [MenuItem("KOL/build/Build Pre")]
+    public static void Preproccess()
+    {
+
+    }
+}
